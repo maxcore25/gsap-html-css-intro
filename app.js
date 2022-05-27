138e7effc5e283b2e226340b35b6904fc2ceaed1
maxcore25 <mkorobov2002@mail.ru>
2022-05-27 06:46:25 +0300

    Links And Sidebars Animations Written

--- app.js
+++ app.js
@@ -1,1 +1,4 @@
 gsap.from('.header', { duration: 1, y: '-100%', ease: 'bounce' });
+gsap.from('.link', { duration: 1, opacity: 0, delay: 1, stagger: 0.5 });
+gsap.from('.right', { duration: 1, x: '-100vw', delay: 1, ease: 'power2.in' });
+gsap.from('.left', { duration: 1, x: '-100%', delay: 1.5 });
