8b159257e0d3e720201eec16c638fb66aa194e17
maxcore25 <mkorobov2002@mail.ru>
2022-05-27 07:03:07 +0300

    Button Animation Written

--- app.js
+++ app.js
@@ -4,3 +4,9 @@ gsap.from('.right', { duration: 1, x: '-100vw', delay: 1, ease: 'power2.in' });
 gsap.from('.left', { duration: 1, x: '-100%', delay: 1.5 });
 
 gsap.to('.footer', { duration: 1, y: 0, ease: 'elastic', delay: 2.5 });
+
+gsap.fromTo(
+  '.button',
+  { opacity: 0, scale: 0, rotation: 720 },
+  { duration: 1, opacity: 1, scale: 1, rotation: 0, delay: 3.5 }
+);
