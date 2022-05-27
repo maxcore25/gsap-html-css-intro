8c602851a6a532d334246c0874792144db7d2bae
maxcore25 <mkorobov2002@mail.ru>
2022-05-27 07:34:22 +0300

    GSAP Animation Timeline Written

--- app.js
+++ app.js
@@ -24,4 +24,17 @@ timeline
   .from('.header', { y: '-100%', ease: 'bounce' })
   .from('.link', { opacity: 0, stagger: 0.5 })
   .from('.right', { x: '-100vw', ease: 'power2.in' }, 1)
-  .from('.left', { x: '-100%' });
+  .from('.left', { x: '-100%' }, '<.5')
+  .to('.footer', { y: 0, ease: 'elastic' })
+  .fromTo(
+    '.button',
+    { opacity: 0, scale: 0, rotation: 720 },
+    { opacity: 1, scale: 1, rotation: 0 }
+  );
+
+// * Reverse Timeline Function
+const button = document.querySelector('.button');
+button.addEventListener('click', () => {
+  timeline.timeScale(3);
+  timeline.reverse();
+});
